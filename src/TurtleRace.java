02f1abfc0aba1df9197cb1b00270697b2f012dee
Bernt Christensen <redheadmail@gmail.com>
2011-10-25 16:05:27 +0200

    The race will not start until you mouse click.

--- src/TurtleRace.java
+++ src/TurtleRace.java
@@ -13,6 +13,7 @@ public class TurtleRace {
 		Turtle t2 = new Turtle(w, 0, 0);
 		track.draw(w);
 		RacingEvent event = new RacingEvent(track, t1, t2);
+		w.waitForMouseClick();
 		event.race();
 	}
 }
